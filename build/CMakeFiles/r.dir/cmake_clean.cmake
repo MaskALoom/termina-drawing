0102fdaa64c1440c5e4d0daa30d2e675e0331995
MaskALoom <maskaloom@gmail.com>
2026-09-17 18:58:59 -0600

    Initial

---
+++ build/CMakeFiles/r.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/r.dir/src/main.cpp.o"
+  "CMakeFiles/r.dir/src/main.cpp.o.d"
+  "r"
+  "r.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/r.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+//#include "raylib.h"
+#include <SDL3/SDL.h>
+
+int main(int argc, char** argv){
+    std::cout << "Something?" << std::endl;
+    return 0;
+}
